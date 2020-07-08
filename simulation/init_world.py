0dc4cadfa4c89a0c2109f8d7bd85f80e16dcd4a5
zirun gan <676580542@qq.com>
2020-07-08 17:51:39 +0100

    add create label.csv

--- simulation/init_world.py
+++ simulation/init_world.py
@@ -1,5 +1,9 @@
+import os
+import pandas
 import numpy as np
 from simulation.environment import Environment
+from simulation.environment import DIRECTORY
+from simulation.environment import DATA_FOLDER_NAME
 
 # the parameter of cloth
 table_position = np.array([0, -0.75, 0])
@@ -13,3 +17,13 @@ cloth_line_color = np.array([0, 0.5, 0, 1])
 environment = Environment(table_position, cloth_position)
 environment.simulate()
 # environment.test()
+
+# generate labels
+data_dir = sorted(os.listdir(os.path.join(DIRECTORY, DATA_FOLDER_NAME, 'bin')))
+path_list = []
+for path in data_dir:
+    parameters = path.replace('.npz', '\n').replace('_', ',')
+    print(parameters)
+    path_list.append(parameters)
+with open('./data/label.csv', 'w') as fin:
+    fin.writelines(path_list)
