16a9bf8c2bfba1a167c2acfc56ebd610f1dd4237
zirun gan <676580542@qq.com>
2020-07-12 21:22:50 +0100

    add save image function

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
+import os
+
 import torch
+import imageio
 
 IMAGE_SIZE = 3
 DATA_SIZE = 1
@@ -18,3 +21,17 @@ def save_checkpoint(state, is_best, filename, best_filename):
     torch.save(state, filename)
     if is_best:
         torch.save(state, best_filename)
+
+
+def save_image(sample_dir, data, label):
+    """
+
+    :param sample_dir:
+    :param data:
+    :param label:
+    :return:
+    """
+    for image in data:
+        image_dir = os.path.join(sample_dir, "{:.1f}_{:.1f}_{:.1f}_{:.0f}_{:.0f}".format(label[0], label[1],
+                                                                                         label[2], label[3], label[4]))
+        imageio.imwrite(image_dir, image)
