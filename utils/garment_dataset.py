49b7b3cd0b942727e5d6430f4da483c68064b113
zirun gan <676580542@qq.com>
2020-07-12 21:23:07 +0100

    change data format

--- utils/garment_dataset.py
+++ utils/garment_dataset.py
@@ -1,15 +1,17 @@
-import pandas as pd
-import torch
 import os
+
 import imageio
 import numpy as np
+import pandas as pd
+import torch
 from torch.utils.data import Dataset
 
 
 class GarmentDataset(Dataset):
     """ Garment dataset"""
 
-    def __init__(self, data_dir, data_type, csv_dir, transform=None):
+    def __init__(self, rgb_dir, data_dir, data_type, csv_dir, transform=None):
+        self.rgb_dir = rgb_dir
         self.data_dir = data_dir
         self.data_type = data_type
         self.labels = pd.read_csv(csv_dir)
@@ -22,14 +24,21 @@ class GarmentDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.to_list()
 
-        label = self.labels.iloc[idx]
+        label = np.array(self.labels.iloc[idx])
+        sample = None
 
-        data_name = "{}_{}_{}_{:.0f}_{:.0f}.npz".format(label[0], label[1], label[2], label[3], label[4])
-        data_name = os.path.join(self.data_dir, data_name)
-        data = np.load(data_name)
-        sample = data[self.data_type]
+        if self.data_type == 'rgb':
+            img_name = "{}_{}_{}_{:.0f}_{:.0f}.png".format(label[0], label[1], label[2], label[3], label[4])
+            img_name = os.path.join(self.rgb_dir, img_name)
+            img = imageio.imread(img_name)
+            sample = np.asarray(img)
+        elif self.data_type in ['raw_depth', 'depth', 'segmentation']:
+            data_name = "{}_{}_{}_{:.0f}_{:.0f}.npz".format(label[0], label[1], label[2], label[3], label[4])
+            data_name = os.path.join(self.data_dir, data_name)
+            data = np.load(data_name)
+            sample = np.expand_dims(data[self.data_type], axis=0)
 
         if self.transform:
             sample = self.transform(sample)
 
-        return sample
+        return {'sample': sample, 'label': label}
